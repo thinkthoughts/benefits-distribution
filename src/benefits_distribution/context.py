698adf98be2e84ed437bf6ec41c80e819606e7a9
dan_hawkley <consensusmechanics@gmail.com>
2026-07-14 15:26:47 -0600

    Refactor RepositoryContext to use Tuple type hints

--- src/benefits_distribution/context.py
+++ src/benefits_distribution/context.py
@@ -1,30 +1,41 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 
-@dataclass(frozen=True)
+@dataclass
 class RepositoryContext:
+
     repository: str
     repository_description: str
+
+    engineering_statement_title: str
     engineering_statement: str
 
-    grammar: tuple[str, ...]
+    grammar_title: str
+    grammar: Tuple[str, ...]
 
     constraint: str
     connected_lane: str
 
-    engineering_objects: tuple[str, ...]
-    engineering_variables: tuple[str, ...]
-    observable_states: tuple[str, ...]
-    indicators: tuple[str, ...]
+    engineering_objects: Tuple[str, ...]
+    engineering_variables: Tuple[str, ...]
+    observable_states: Tuple[str, ...]
+    indicators: Tuple[str, ...]
 
-    lane_symbols: tuple[str, ...]
-    lane_labels: tuple[str, ...]
+    lane_symbols: Tuple[str, ...]
+    lane_labels: Tuple[str, ...]
 
     repository_variable_title: str
     lane_caption: str
 
-    construction_sequence: tuple[str, ...]
+    repository_sequence_title: str
+    repository_sequence_caption: str
+
+    construction_sequence: Tuple[str, ...]
+
+    design_principle: str
 
     next_notebook: str
-    source: str = ""
-    notes: str = ""
+
+    source: str
+    notes: str
